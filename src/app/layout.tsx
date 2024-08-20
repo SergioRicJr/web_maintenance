15b8d36d4e7da252c81c0335784f95be54f08dd7
SergioNascimento07 <sergioricjr7@gmail.com>
2024-08-19 22:35:30 -0300

    feat: next-themes added to the project, functionality and toggle theme created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/modeToggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <ModeToggle className="fixed right-8 bottom-6" />
+          </ThemeProvider>
+      </body>
     </html>
   );
 }
