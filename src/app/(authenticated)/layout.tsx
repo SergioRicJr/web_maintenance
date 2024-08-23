fe050c72b63a23b3217c994ecbf524f20ae489f1
Sergio <sergioricjr7@gmail.com>
2024-08-23 10:39:39 -0300

    refactor: header component created and separated

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -5,6 +5,7 @@ import {
     MenubarMenu,
 } from "@/components/ui/menubar"
 import { Button } from "@/components/ui/button";
+import Header from "@/components/header";
 
 interface PrivateLayoutProps {
     children: ReactNode;
@@ -14,18 +15,7 @@ export default async function PrivateLayout({ children }: PrivateLayoutProps) {
 
     return (
         <div className="min-h-screen w-screen flex flex-col">
-            <header className="w-full p-4 flex justify-between">
-                <MaintenanceLogo size={195}/>
-                <Menubar className="h-12">
-                    <MenubarMenu>
-                        <Button variant='ghost'>Máquinas</Button>
-                        <Button variant='ghost'>Peças</Button>
-                        <Button variant='ghost'>Manutenções</Button>
-                        <Button variant='ghost'>Equipes</Button>
-                        <Button variant='ghost'>Dashboard</Button>
-                    </MenubarMenu>
-                </Menubar>
-            </header>
+            <Header/>
             {children}
         </div>
     );
